5c48bbfc6d3fc5dc7ea761b5f2491953768c187e
Emmanuel Omonzebaguan <codeplan73@gmail.com>
2023-05-04 11:40:01 +0100

    implemented login and logout

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,41 +1,64 @@
-const User = require('../models/User');
-const {StatusCodes} = require('http-status-codes');
+const User = require('../models/User')
+const { StatusCodes } = require('http-status-codes')
 const CustomError = require('../errors')
-const { attachCookiesToResponse, createTokenUser} = require('../utils')
+const { attachCookiesToResponse, createTokenUser } = require('../utils')
 
 const register = async (req, res) => {
-    const {email, name, password} = req.body;
+  const { email, name, password } = req.body
 
-    const emailAlreadyExists = await User.findOne({email});
-    if(emailAlreadyExists){
-        throw new CustomError.BadRequestError('Email already exist')
-    }
+  if (!email || !name || !password) {
+    throw new CustomError.BadRequestError('All fields required')
+  }
 
-    // assign admin role to first account
-    const isFirstAccount = (await User.countDocuments({})) === 0;
-    const role = isFirstAccount ? "admin" : 'user';
+  const emailAlreadyExists = await User.findOne({ email })
+  if (emailAlreadyExists) {
+    throw new CustomError.BadRequestError('Email already exist')
+  }
 
-    const user = await User.create({ email, name, password});
-    const tokenUser = {name: user.name, userId: user._id, role: user.role};
+  // assign admin role to first account created
+    const isFirstAccount = (await User.countDocuments({})) === 0
+    const role = isFirstAccount ? "admin" : "user";
 
-    attachCookiesToResponse({res, user: tokenUser});
-    res.status(StatusCodes.OK).json(tokenUser);
 
+    const user = await User.create({ name, email, password, role });
+    const tokenUser = { name: user.name, userId: user._id, role: user.role }
+
+  attachCookiesToResponse({ res, user: tokenUser })
+  res.status(StatusCodes.OK).json(tokenUser)
 }
 
 const login = async (req, res) => {
-    res.send('login user account')
+  const { email, password } = req.body
+
+  if (!email || !password) {
+    throw new CustomError.BadRequestError('Please provide email and password')
+  }
+
+  const user = await User.findOne({ email })
+  if (!user) {
+    throw new CustomError.UnauthenticatedError('Invalid credentials')
+  }
+
+  const isPasswordCorrect = await user.comparePassword(password)
+  if (!isPasswordCorrect) {
+    throw new CustomError.UnauthenticatedError('Invalid credentials')
+  }
+
+  const tokenUer = createTokenUser(user)
+  attachCookiesToResponse({ res, user: tokenUer })
+  res.status(StatusCodes.OK).json(tokenUer)
 }
 
 const logout = async (req, res) => {
-    res.cookie('token', 'logout', {
-        httpOnly: true,
-        expires: new Date(Date.now() + 5 * 1000)
-    });
-    res.status(StatusCodes.OK).json({ msg: 'user logged out'});
+  res.cookie('token', 'logout', {
+    httpOnly: true,
+    expires: new Date(Date.now() + 5 * 1000),
+  })
+  res.status(StatusCodes.OK).json({ msg: 'user logged out' })
 }
 
 module.exports = {
-    register, login, logout
+  register,
+  login,
+  logout,
 }
-
