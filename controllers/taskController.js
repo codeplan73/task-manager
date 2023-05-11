534325a801117d2d43af406a57e58a64f274837e
Emmanuel Omonzebaguan <codeplan73@gmail.com>
2023-05-11 15:13:52 +0100

    create task

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -1,9 +1,20 @@
+const { StatusCodes } = require('http-status-codes')
+const CustomError = require('../errors')
+const Task  = require('./../models/Task')
+
 const createTask = async (req, res) => {
-    res.send('admin create task');
+    const { assignedBy, dueDate, tag, description, project } = req.body
+  if (!assignedBy || !dueDate || !tag || !description || !project) {
+    throw new CustomError.BadRequestError('All fields required')
+  }
+
+  const task = await Task.create({ assignedBy, dueDate, tag, description, project })
+  res.status(StatusCodes.OK).json({ task })
 }
 
 const getAllTasks = async (req, res) => {
-    res.send('get all tasks');
+    const tasks = await Task.find({})
+  res.status(StatusCodes.OK).json({ tasks, count: tasks.length })
 }
 
 const getTask = async (req, res) => {
