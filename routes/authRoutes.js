3cf49cf25ab6bd19baa7f208f3c04984de2b87c8
Emmanuel Omonzebaguan <97120421+codeplan73@users.noreply.github.com>
2023-06-16 12:35:05 +0100

    Update authRoutes.js
    Modify auth route

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const {register, login, logout} = require('./../controllers/authController')
 const { authenticateUser, authorizePermissions } = require('../middleware/authentication');
 
-router.post('/', [authenticateUser, authorizePermissions('admin')], register);
+// router.post('/', [authenticateUser, authorizePermissions('admin')], register);
 router.post('/login', login);
 router.get('/logout', logout);
 
