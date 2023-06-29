5c9d412ea0ca72e586df56a4f35e21742cba981f
Emmanuel Omonzebaguan <codeplan73@gmail.com>
2023-06-29 16:19:17 +0100

    updated auth

--- app.js
+++ app.js
@@ -1,48 +1,47 @@
-require('dotenv').config();
-require('express-async-errors');
-const express = require('express');
-const morgan = require('morgan');
-const cookieParser = require('cookie-parser');
-const cors = require('cors');
-const corsOptions ={
-    origin:'http://localhost:5173', 
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200
-}
+require("dotenv").config();
+require("express-async-errors");
+const express = require("express");
+const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const corsOptions = {
+  origin: "http://localhost:5173",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
 
-const app = express()
+const app = express();
 app.use(cors(corsOptions));
 
 // connect db
-const connectDB = require('./db/connect');
+const connectDB = require("./db/connect");
 
 // routes
-const authRoutes = require('./routes/authRoutes');
-const userRoutes = require('./routes/userRoutes');
-const projectRoutes = require('./routes/projectRoutes')
-const taskRoutes = require('./routes/taskRoutes')
+const authRoutes = require("./routes/authRoutes");
+const userRoutes = require("./routes/userRoutes");
+const projectRoutes = require("./routes/projectRoutes");
+const taskRoutes = require("./routes/taskRoutes");
 
 // app use package
 app.use(express.json());
-app.use(morgan('tiny'));
+app.use(morgan("tiny"));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser(process.env.JWT_SECRET));
 
-app.use('/api/v1/auth', authRoutes)
-app.use('/api/v1/users', userRoutes)
-app.use('/api/v1/projects', projectRoutes)
-app.use('/api/v1/tasks', taskRoutes)
- 
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/users", userRoutes);
+app.use("/api/v1/projects", projectRoutes);
+app.use("/api/v1/tasks", taskRoutes);
 
 const port = process.env.PORT;
 
 const start = async () => {
-    try {
-        await connectDB(process.env.MONGO_URI)
-        app.listen(port, console.log(`Server runing on port ${port}`))
-    } catch (error) {
-        console.log(error)
-    }
-}
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`Server runing on port ${port}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 start();
